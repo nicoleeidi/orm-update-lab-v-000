53c19a4c0fc23c079a85f41a28446e0d6af7dc53
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-06 17:15:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,9 +19,9 @@ class Student
       self.update
     else
       DB[:conn].execute("INSERT INTO students (name,grade) VALUES (?,?)",self.name,self.grade)
-    end 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-  end 
+    end
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
